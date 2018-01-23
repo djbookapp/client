b6510ff25bb80d0163cd2c34fcf889efd117b2cb
Dave Snowberger <git@fartherout.org>
2018-01-22 17:25:04 -0800

    slim jquery apparently does not support .get

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const __API_URL__ = 'https://djbookapp.herokuapp.com';
 var $content = $('.content');
 
 $.get(`${__API_URL__}/api/db`, data=> {
-  $content.append(data));
+  $content.append(data);
 });
