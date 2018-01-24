06ca501565332a462da8db78853d4cad97f5ac50
Dave Snowberger <git@fartherout.org>
2018-01-23 21:02:35 -0800

    fixing the views

--- scripts/views/addbook-view.js
+++ scripts/views/addbook-view.js
@@ -1,13 +1,24 @@
 'use strict';
 var app = app || {};
 
-(function(module) {
+(function(module, Book) {
   var addBookView = {};
-
-  addBookView.initAddBookPage = () => {
+  
+  addBookView.initAddBook = () => {
     $('.container').hide();
     $('.addbook-view').show();
+    $('#add-book').on('submit', e => {
+      e.preventDefault();
+      let book = new Book({
+        author: $('#book-author').val(),
+        title: $('#book-title').val(),
+        isbn: $('#book-isbn').val(),
+        imgUrl: $('#book-img-url').val(),
+        description: $('#book-description').val()
+      });
+      book.addBook();
+    });
   }
 
   module.addBookView = addBookView;
-})(app)
+})(app, app.Book)
