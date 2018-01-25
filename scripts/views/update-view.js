50fc9c5f488d9141a7899cc8c04344f575bcc860
Dave Snowberger <git@fartherout.org>
2018-01-25 11:16:55 -0800

    update and delete functionality

--- scripts/views/update-view.js
+++ scripts/views/update-view.js
@@ -10,11 +10,25 @@ var app = app || {};
                 book = el;
             }
         });
+
         var template = Handlebars.compile($('#update-template').text());
         $('.container').addClass('hidden');
         $('.update-view').empty();
         $('.update-view').removeClass('hidden');
         $('.update-view').append(template(book));
+
+        $('#updatebookbutton').on('submit', e => {
+          e.preventDefault();
+          book.author = $('#book-author').val();
+          book.title = $('#book-title').val();
+          book.isbn = $('#book-isbn').val();
+          book.imgUrl = $('#book-img-url').val();
+          book.description = $('#book-description').val();
+
+          book.update();
+          page.show(`/books/${book.id}`);
+        });
+
     }
 
     module.updateView = updateView;
