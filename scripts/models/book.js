360cfd52aab744a76b984f7ed1b8146efecaa83d
Dave Snowberger <git@fartherout.org>
2018-01-25 11:59:23 -0800

    working on the update view

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -21,7 +21,13 @@ var app = app || {};
   }
 
   Book.prototype.update = function() {
-    $.put(`${__API_URL__}/api/db/${this.id}`, {author: this.author, title: this.title, isbn: this.isbn, imgUrl: this.imgUrl, description: this.description});
+    $.ajax({
+      url: `${__API_URL__}/api/db/${this.book_id}`,
+      method: 'PUT',
+      contentType: 'application/json',
+      data: {author: this.author, title: this.title, isbn: this.isbn, imgUrl: this.imgUrl, description: this.description},
+    })
+    .then(console.log(this.book_id));
   }
 
   Book.prototype.destroy = function() {
