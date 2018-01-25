a35de99489ba8e64737826161dff148ff95253a2
IFREECAN <jasper.kinnay@gmmail.com>
2018-01-25 10:05:44 -0800

    worked on update view

--- scripts/views/update-view.js
+++ scripts/views/update-view.js
@@ -3,7 +3,18 @@
 var app = app || {};
 (function(module) {
     var updateView = {};
-    updateView.init = () => {
-        
+    updateView.init = (ctx,next) => {
+        var book = {};
+        app.Book.books.forEach(el => {
+            if (el.id === ctx.id) {
+                book = el;
+                break;
+            }
+        });
+        var template = Handlebars.compile($('#update-template').text());
+        $('.container').addClass('hidden');
+        $('.update-view').empty();
+        $('.update-view').removeClass('hidden');
+        $('.update-view').append(template(book));
     };
 })(app)
